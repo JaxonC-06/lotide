e6b46d47b620d401615593ab5df79c0335e498d0
JaxonC-06 <jaxoncostain@outlook.com>
2025-02-21 18:01:13 -0700

    Removed the for function and added various test cases.

--- tail.js
+++ tail.js
@@ -8,12 +8,6 @@ const assertEqual = function(actual, expected) {
 
 const tail = function(arr) {
   const newArr = arr.slice(1);
-
-  for (const index of arr) {
-    if (index === 0 || index === 1) {
-      return [];
-    }
-  }
   return newArr;
 };
 
@@ -21,3 +15,8 @@ const tail = function(arr) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+assertEqual(tail(words).length, 2); // This test checks the length of the returned array.
+
+assertEqual(tail(["Test"]).length, 0); //This checks if an array with only one element returns as empty, which it should.
+assertEqual(tail([]).length, 0); // This checks if an array with zero elements returns as empty, which it also should.

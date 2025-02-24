3481c63681557d8e35567daf6f2de7ad12261f1c
JaxonC-06 <jaxoncostain@outlook.com>
2025-02-23 17:52:04 -0700

    Added the assertArraysEqual function.

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(firstArr, secondArr) {
+  if (firstArr.length !== secondArr.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  if (eqArrays(firstArr, secondArr)) {
+    console.log("The arrays are equal.")
+  } else {
+    console.log("The arrays are different.")
+  }
+};

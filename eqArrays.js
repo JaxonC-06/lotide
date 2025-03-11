332430aeafd5210e63ffa862322aa6c21b9bb6f3
JaxonC-06 <jaxoncostain@outlook.com>
2025-03-11 14:47:08 -0600

    Refractored eqArrays.js, added test file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(firstArr, secondArr) {
   if (firstArr.length !== secondArr.length) {
     return false;
@@ -19,5 +11,4 @@ const eqArrays = function(firstArr, secondArr) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+module.exports = eqArrays;

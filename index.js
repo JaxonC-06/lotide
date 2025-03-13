e3987a3567f281a56ffa8d8d1dbf7bb0e5907180
JaxonC-06 <jaxoncostain@outlook.com>
2025-03-13 13:46:36 -0600

    Removed map and flatten from the exported functions.

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const eqArrays = require("./eqArrays.js");
 const eqObjects = require("./eqObjects.js");
 const findKey = require("./findKey.js");
 const findKeyByValue = require("./findKeyByValue.js");
-const flatten = require("./flatten.js");
 const head = require("./head.js");
 const letterPositions = require("./letterPositions.js");
-const map = require("./map.js");
 const middle = require("./middle.js");
 const tail = require("./tail.js");
 const takeUntil = require("./takeUntil.js");
@@ -26,10 +24,8 @@ module.exports = {
   eqObjects: eqObjects,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
-  flatten: flatten,
   head: head,
   letterPositions: letterPositions,
-  map: map,
   middle: middle,
   tail: tail,
   takeUntil: takeUntil,
